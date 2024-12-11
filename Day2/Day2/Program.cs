56f6ec8a627b05f8969964e996bf7d954ec5c691
FraneMaric <frane.maric@led-elektronika.hr>
2024-12-11 14:52:58 +0100

    Day 2 part 1

--- Day2/Day2/Program.cs
+++ Day2/Day2/Program.cs
@@ -1,7 +1,83 @@
-﻿class Program
+﻿
+class Program
 {
     static void Main(string[] args)
     {
-        string content = File.ReadAllText("")
+        string input = File.ReadAllText("C:\\Users\\maric\\OneDrive\\Desktop\\AOC\\AOC-2024\\Day2\\Day2Inputs.txt");
+        string[] lines = input.Split("\n");
+        int result = 0;
+        foreach (var line in lines)
+        {
+            result += TestLine(line);
+        }
+        Console.WriteLine("There are " + result + " safe reports");
+    }
+
+    private static int TestLine(string line)
+    {
+        int counter = 0;    //Counter for safe reports
+        bool safe;
+        try
+        {
+            List<int> reports = new List<int>();
+
+            foreach (var item in line.Split(" "))
+            {
+                reports.Add(int.Parse(item));
+            }
+
+            if (reports[1] - reports[0] <= 3 && reports[1] - reports[0] > 0)
+            {
+                safe = ReportsIncrease(reports);
+                if (safe == true)
+                {
+                    counter++;
+                }
+            }
+            else if (reports[0] - reports[1] <= 3 && reports[0] - reports[1] > 0)
+            {
+                safe = ReportsDecrease(reports);
+                if (safe == true)
+                {
+                    counter++;
+                }
+            }
+            else
+            {
+                //Not safe
+            };
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+
+        return counter;
+    }
+
+    private static bool ReportsIncrease(List<int> reports)
+    {
+        bool safe = true;
+        for (int i = 0; i < reports.Count - 1; i++)
+        {
+            if (reports[i + 1] - reports[i] > 3 || reports[i + 1] - reports[i] <= 0)
+            {
+                safe = false;
+            }
+        }
+        return safe;
+    }
+
+    private static bool ReportsDecrease(List<int> reports)
+    {
+        bool safe = true;
+        for (int i = 0; i < reports.Count - 1; i++)
+        {
+            if (reports[i] - reports[i + 1] > 3 || reports[i] - reports[i + 1] <= 0)
+            {
+                safe = false;
+            }
+        }
+        return safe;
     }
 }
--- Day2/Day2/obj/Debug/net8.0/Day2.AssemblyInfo.cs
+++ Day2/Day2/obj/Debug/net8.0/Day2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Day2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b258a8949f1ec0c306be52f30258c5409a5dc5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Day2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Day2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
