77369dbd0e83e67e3015da2c3f1390c63c98ca3a
FraneMaric <maric.frane@gmail.com>
2024-12-26 01:20:22 +0100

    Day3 both parts complete

--- Day2/Day2/Program.cs
+++ Day2/Day2/Program.cs
@@ -1,83 +1,124 @@
 ﻿
+
 class Program
 {
     static void Main(string[] args)
     {
-        string input = File.ReadAllText("C:\\Users\\maric\\OneDrive\\Desktop\\AOC\\AOC-2024\\Day2\\Day2Inputs.txt");
+        string input = File.ReadAllText("D:\\Programs\\AOC-2024\\AOC-2024\\Day2\\Day2Inputs.txt");
         string[] lines = input.Split("\n");
         int result = 0;
+        bool safe;
+        Dictionary<string, bool>dic = new Dictionary<string, bool>();
         foreach (var line in lines)
         {
-            result += TestLine(line);
+            dic.Add(line,false);
         }
+
         Console.WriteLine("There are " + result + " safe reports");
     }
 
-    private static int TestLine(string line)
+    
+
+    private static bool TestLine(string line)
     {
-        int counter = 0;    //Counter for safe reports
-        bool safe;
-        try
-        {
-            List<int> reports = new List<int>();
-
-            foreach (var item in line.Split(" "))
-            {
-                reports.Add(int.Parse(item));
-            }
-
-            if (reports[1] - reports[0] <= 3 && reports[1] - reports[0] > 0)
-            {
-                safe = ReportsIncrease(reports);
-                if (safe == true)
-                {
-                    counter++;
-                }
-            }
-            else if (reports[0] - reports[1] <= 3 && reports[0] - reports[1] > 0)
-            {
-                safe = ReportsDecrease(reports);
-                if (safe == true)
-                {
-                    counter++;
-                }
-            }
-            else
-            {
-                //Not safe
-            };
+        try{
+            string[] splitLine=line.Split(" ");
+        int[] report=new int[10];
+        for(int i=0;i<splitLine.Length;i++){
+            report[i]=Int32.Parse(splitLine[i]);
         }
-        catch (Exception e)
-        {
+        Console.WriteLine(report[1]);
+        return true;
+        }catch(Exception e){
             Console.WriteLine(e);
         }
-
-        return counter;
+        return true;
     }
 
-    private static bool ReportsIncrease(List<int> reports)
-    {
-        bool safe = true;
-        for (int i = 0; i < reports.Count - 1; i++)
-        {
-            if (reports[i + 1] - reports[i] > 3 || reports[i + 1] - reports[i] <= 0)
-            {
-                safe = false;
-            }
-        }
-        return safe;
-    }
 
-    private static bool ReportsDecrease(List<int> reports)
-    {
-        bool safe = true;
-        for (int i = 0; i < reports.Count - 1; i++)
-        {
-            if (reports[i] - reports[i + 1] > 3 || reports[i] - reports[i + 1] <= 0)
-            {
-                safe = false;
-            }
-        }
-        return safe;
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // private static int TestLine(string line)
+    // {
+    //     int counter = 0;    //Counter for safe reports
+    //     bool safe;
+    //     try
+    //     {
+    //         List<int> reports = new List<int>();
+
+    //         foreach (var item in line.Split(" "))
+    //         {
+    //             reports.Add(int.Parse(item));
+    //         }
+
+    //         if (reports[1] - reports[0] <= 3 && reports[1] - reports[0] > 0)
+    //         {
+    //             safe = ReportsIncrease(reports);
+    //             if (safe == true)
+    //             {
+    //                 counter++;
+    //             }
+    //         }
+    //         else if (reports[0] - reports[1] <= 3 && reports[0] - reports[1] > 0)
+    //         {
+    //             safe = ReportsDecrease(reports);
+    //             if (safe == true)
+    //             {
+    //                 counter++;
+    //             }
+    //         }
+    //         else
+    //         {
+    //             //Not safe
+    //         };
+    //     }
+    //     catch (Exception e)
+    //     {
+    //         Console.WriteLine(e);
+    //     }
+
+    //     return counter;
+    // }
+
+    // private static bool ReportsIncrease(List<int> reports)
+    // {
+    //     bool safe = true;
+    //     for (int i = 0; i < reports.Count - 1; i++)
+    //     {
+    //         if (reports[i + 1] - reports[i] > 3 || reports[i + 1] - reports[i] <= 0)
+    //         {
+    //             safe = false;
+    //         }
+    //     }
+    //     return safe;
+    // }
+
+    // private static bool ReportsDecrease(List<int> reports)
+    // {
+    //     bool safe = true;
+    //     for (int i = 0; i < reports.Count - 1; i++)
+    //     {
+    //         if (reports[i] - reports[i + 1] > 3 || reports[i] - reports[i + 1] <= 0)
+    //         {
+    //             safe = false;
+    //         }
+    //     }
+    //     return safe;
+    // }
 }
---
+++ Day3/Day3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = File.ReadAllText("D:\\Programs\\AOC-2024\\AOC-2024\\Day3\\Day3\\Day3Input.txt");
+        string pattern = @"do\(\)|don't\(\)|mul\((\d+),(\d+)\)";
+        int result=0;
+        bool isEnabled=true;
+
+        MatchCollection matches = Regex.Matches(input, pattern);
+
+        List<int> numbers = new List<int>();
+        foreach (Match match in matches)
+        {
+            switch(match.Value){
+            case "do()":
+                isEnabled = true;
+                break;
+            case "don't()":
+                isEnabled = false;
+                break;
+            default:
+            {
+                if (match.Value.StartsWith("mul") && isEnabled)
+                {
+                    numbers.Add(multiply(match.Value));
+                }
+
+                break;
+            }
+            }
+        }
+        
+        foreach(int number in numbers){
+            result+=number;
+        }
+
+        Console.WriteLine(result);
+    }
+
+    private static int multiply(string value)
+    {
+        int x,y;
+        x=Convert.ToInt32(value.Substring(4,value.IndexOf(",")-4));
+        y=Convert.ToInt32(value.Substring(value.IndexOf(",")+1,value.IndexOf(")")-value.IndexOf(",")-1));
+
+        int result=x*y;
+        return result;
+    }
+}
